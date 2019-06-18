68d340995a2d81255669a7884e1cef21b5e86072
amariampolskiy <amariampolskiy@gmail.com>
2019-06-18 11:49:26 +0300

    add message to locked screen

--- config.h
+++ config.h
@@ -3,10 +3,19 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#262626",     /* after initialization */
+	[INIT] =   "#262626",   /* after initialization */
 	[INPUT] =  "#5F87AF",   /* during input */
 	[FAILED] = "#AF5F5F",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 0;
+
+/* default message */
+static const char *message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char *text_color = "#BCBCBC";
+
+/* text size (must be a valid size) */
+static const char *text_name = "*liberation mono-medium-r*16";
